a40cf9451222ddcf3413c9aebdc1ab86d52619c1
arun <akhatri@asite.com>
2024-05-19 18:31:50 +0530

    get api with header pass in json code committed

---
+++ src/tests/srj.spec.js
@@ -0,0 +1,25 @@
+import { test, expect, request } from "@playwright/test";
+/*
+test('API Get Request', async ({ request }) => {
+
+    const respons = await request.get('https://restful-booker.herokuapp.com/booking/:id',{
+        headers:{
+            Accept:"application/json"
+        }
+    })
+    expect(respons.status()).toBe(200)
+    console.log(await respons.json())
+}) */
+
+test('API Get Request', async ({ request }) => {
+
+    const respons = await request.get('https://restful-booker.herokuapp.com/booking/2402',{
+        headers:{
+            Accept:"application/json"
+        }
+    })
+    expect(respons.status()).toBe(200)
+    //const test = await respons.text()
+   // expect(test).toContain('Arun')
+    console.log(await respons.json())
+})
