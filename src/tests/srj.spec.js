eb6660db2e3dddd866777f1ef3e6afc7586854c2
arun <akhatri@asite.com>
2024-05-20 15:43:07 +0530

    Updated Post API Call

--- src/tests/srj.spec.js
+++ src/tests/srj.spec.js
@@ -1,8 +1,10 @@
 import { test, expect, request } from "@playwright/test";
+const xml2js = require('xml2js');
+
 /*
 test('API Get Request', async ({ request }) => {
 
-    const respons = await request.get('https://restful-booker.herokuapp.com/booking/:id',{
+    const respons = await request.post('https://dmsqa.asite.com/apilogin/',{
         headers:{
             Accept:"application/json"
         }
@@ -10,10 +12,9 @@ test('API Get Request', async ({ request }) => {
     expect(respons.status()).toBe(200)
     console.log(await respons.json())
 }) */
-
+/*
 test('API Get Request', async ({ request }) => {
-
-    const respons = await request.get('https://restful-booker.herokuapp.com/booking/2402',{
+   const respons = await request.get('https://restful-booker.herokuapp.com/booking/2402',{
         headers:{
             Accept:"application/json"
         }
@@ -22,4 +23,37 @@ test('API Get Request', async ({ request }) => {
     //const test = await respons.text()
    // expect(test).toContain('Arun')
     console.log(await respons.json())
-})
+}
+)
+*/
+test.only('API Post Request', async ({ request }) => {
+
+    const response = await request.post('https://dmsqa.asite.com/apilogin/', {
+        form: {
+            emailId: 'akhatri@asite.com',
+            password: 'Asite@123',
+          },
+          headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+    })
+    expect(response.status()).toBe(200)
+    const responseXml = await response.text();
+
+    // Convert XML to JSON
+  const parser = new xml2js.Parser();
+  parser.parseString(responseXml, (err, result) => {
+    if (err) {
+      throw new Error('Error parsing XML');
+    }
+    
+    const responseJson = result;
+    //console.log(responseJson)
+
+    var sessionId=responseJson.UserProfile.Sessionid[0];
+    console.log("This is your Session ID " + sessionId)
+
+    
+});
+
+});
